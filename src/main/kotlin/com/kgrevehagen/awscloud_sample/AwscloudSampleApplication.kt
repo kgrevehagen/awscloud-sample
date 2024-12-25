139d7bab04b9e6bf97c85418fd29ca939d84fd23
Kristian Greve Hagen <kristian.greve.hagen@gmail.com>
2024-12-26 00:41:57 +0100

    initial spring boot

---
+++ src/main/kotlin/com/kgrevehagen/awscloud_sample/AwscloudSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.kgrevehagen.awscloud_sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwscloudSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<AwscloudSampleApplication>(*args)
+}
